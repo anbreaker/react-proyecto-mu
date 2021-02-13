ad2296468656a1fb64fdf1da6f186f5a0e7e692e
anbreaker <antunez19@gmail.com>
2021-02-13 21:07:18 +0100

    agregadas traducciones a NotFoundPage

--- src/components/pages/NotFoundPage.js
+++ src/components/pages/NotFoundPage.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
+import { ChangeLanguaje } from '../utils/ChangeLanguaje';
 
 export const NotFoundPage = () => {
+  const { t } = useTranslation('global');
+
   return (
     <>
       <div id="content-wrapper" className="mt-xl-5 d-flex flex-column">
@@ -13,14 +18,12 @@ export const NotFoundPage = () => {
               <div className="error mx-auto" data-text="404">
                 404
               </div>
-              <p className="lead text-gray-800 mb-5">Página no encontrada</p>
-              <p className="text-gray-500 mb-0">
-                Ouch! Parece que has encontrado un fallo en matrix...
+              <p className="lead text-gray-800 mb-5">
+                {t('NotFoundPage.Not-Found')}
               </p>
+              <p className="text-gray-500 mb-0">{t('NotFoundPage.Matrix')}</p>
 
-              <Link to="/">
-                &larr; Vuelve al inicio de la web, para el ¡rescate!
-              </Link>
+              <Link to="/">&larr; {t('NotFoundPage.Go-Back')}</Link>
             </div>
           </div>
 
@@ -28,12 +31,13 @@ export const NotFoundPage = () => {
           <footer className="sticky-footer bg-white">
             <div className="container my-auto">
               <div className="copyright text-center my-auto">
-                <span>Aplicacion web hecha con ❤️‍🔥 por Talent-Mu.</span>
+                <span>{t('NotFoundPage.Made-With')}</span>
               </div>
             </div>
           </footer>
           {/* <!-- End of Footer --> */}
         </div>
+        <ChangeLanguaje />
       </div>
     </>
   );
