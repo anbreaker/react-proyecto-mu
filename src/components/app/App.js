0b46757a6829e90f642ee9ca0a78dc1dd25956fd
anbreaker <antunez19@gmail.com>
2021-02-18 20:19:42 +0100

    Provider funcionando

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+
 import '../../assets/css/sb-admin-2.min.css';
 import '../../assets/vendor/fontawesome-free/css/all.min.css';
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/storeConfig';
+
 import { I18nextProvider } from 'react-i18next';
 import i18next from 'i18next';
 
@@ -8,10 +11,12 @@ import { App } from './components/app/App';
 import './configs/i18next-config';
 
 ReactDOM.render(
-  <I18nextProvider i18n={i18next}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </I18nextProvider>,
+  <Provider store={store}>
+    <I18nextProvider i18n={i18next}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </I18nextProvider>
+  </Provider>,
   document.getElementById('root')
 );
