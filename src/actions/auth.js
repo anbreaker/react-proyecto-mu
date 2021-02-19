fc0c3c26447cdb7d8d19156c0567f91ea8feb22a
anbreaker <antunez19@gmail.com>
2021-02-19 11:46:13 +0100

    iniciando con redux

---
+++ src/actions/auth.js
@@ -0,0 +1,21 @@
+import { firebaseInit, googleAuthProvider } from '../firebase/firebaseConfig';
+import { types } from '../types/types';
+
+export const startGoogleLogin = () => {
+  return dispatch => {
+    firebaseInit
+      .auth()
+      .signInWithPopup(googleAuthProvider)
+      .then(({ user }) => dispatch(login(user.uid, user.displayName)));
+  };
+};
+
+export const login = (uid, displayName) => {
+  return {
+    type: types.login,
+    payload: {
+      uid,
+      displayName,
+    },
+  };
+};
