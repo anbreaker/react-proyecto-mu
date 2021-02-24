c6b8e30181b29d6e72958a743c8a14eb6ba586d9
Luis Sanchez <luissanchez_1992@hotmail.com>
2021-02-24 15:25:10 +0100

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+checkCode(){
+  if [[ $? -gt 0 ]]
+  then
+    echo "Ocurrio un error"
+    exit 1
+  fi
+}
+
 echo "Build React APP"
 npm run build
-echo "copy build in server"
-scp -r build MU-NGINX:react-app/ 
+checkCode
+echo "Copy build in server"
+scp -r build MU-NGINX:react-app/
+checkCode
 ssh MU-NGINX "cd react-app;ls;sudo systemctl restart nginx.service;systemctl status nginx.service"
+checkCode
 echo "This is the end, my only friend"
