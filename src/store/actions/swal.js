8fd7718638c00447b42a4a8015067dd52b0e3150
anbreaker <antunez19@gmail.com>
2021-03-02 20:27:20 +0100

    Parametros para mejorar el Swal

--- src/store/actions/swal.js
+++ src/store/actions/swal.js
@@ -4,7 +4,12 @@ import { types } from '../types/types';
  * @param {Example: 'success','error'} type string
  * @param {Mesagge to translate i18n} sms
  * @param {Title to translate i18n Modal} title
+ * @param {showCancelButton: true},
+ * @param {confirmButtonColor: '#E12717'},
+ * @param {cancelButtonColor: '#2E59D9'},
+ * @param {confirmButtonText: 'Yes, delete it!'}),
  */
+
 export const setAlertAction = (title, text, icon) => ({
   type: types.swalSetAction,
   payload: {
