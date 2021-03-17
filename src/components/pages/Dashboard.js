3d81a967895d313040465807ee57fd068c9b2cbe
anbreaker <antunez19@gmail.com>
2021-03-17 21:57:33 +0100

    agregando componente InfoCard, creando Dashboard inicial

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,79 +1,42 @@
 // eslint-disable
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 
 import { MainLayout } from '../layout/MainLayout';
-import { Button } from '../basicComponents/Button';
-import { setAlertAction } from '../../store/actions/swal';
-import Swal from 'sweetalert2';
+import { InfoCards } from '../parts/InfoCards';
 
 export const Dashboard = () => {
-  //! Sólo para pruebas
-  const dispatch = useDispatch();
-
-  const handleClickExito = () => {
-    dispatch(
-      setAlertAction('ErrorSwal.Success', 'ErrorSwal.Welcome', 'success')
-    );
-  };
-
-  const handleClickError = () => {
-    dispatch(
-      setAlertAction(
-        'ErrorSwal.Error',
-        'ErrorSwal.auth/user-not-found',
-        'error'
-      )
-    );
-  };
-
-  const handleClickLoading = () => {
-    Swal.fire({
-      title: 'Uploading...',
-      text: 'Please wait...',
-      allowOutsideClick: false,
-      showConfirmButton: false,
-      willOpen: () => {
-        Swal.showLoading();
-      },
-    });
-
-    setTimeout(() => {
-      Swal.close();
-    }, 1500);
-  };
-  const handleClickDelete = () => {
-    Swal.fire({
-      title: 'Are you sure?',
-      text: "You won't be able to revert this!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#f5365c',
-      cancelButtonColor: '#5E72E4',
-      confirmButtonText: 'Yes, delete it!',
-    }).then(result => {
-      if (result.value) {
-        Swal.fire('Deleted!', 'Your file has been deleted.', 'success');
-      }
-    });
-  };
+  const { t } = useTranslation('global');
 
   return (
     <MainLayout>
       <div className="container-fluid">
-        <h1 className="h3 mb-4 text-gray-800">Blank Page</h1>
-        <Button type="button" variant="primary" onClick={handleClickExito}>
-          Alerta Exito
-        </Button>
-        <Button type="button" variant="alert" onClick={handleClickError}>
-          Alerta Error
-        </Button>
-        <Button type="button" variant="primary" onClick={handleClickLoading}>
-          Loading Example
-        </Button>
-        <Button type="button" variant="alert" onClick={handleClickDelete}>
-          Delete
-        </Button>
+        <h1 className="h3 mb-4 text-gray-800">{t('Dashboard.Welcome')}</h1>
+
+        <p className="h5 mb-4">{t('Dashboard.Resume')}</p>
+
+        <div className="row">
+          <InfoCards
+            text={t('TreasurerResumePage.Income')}
+            variantBorder={'border-bottom-success'}
+            variantText={'text-success'}
+            quantity={'200'}
+          />
+
+          <InfoCards
+            text={t('TreasurerResumePage.Expenses')}
+            variantBorder={'border-bottom-danger'}
+            variantText={'text-danger'}
+            quantity={'200'}
+          />
+
+          <InfoCards
+            text={t('TreasurerResumePage.Net')}
+            variantBorder={'border-bottom-info'}
+            variantText={'text-info'}
+            quantity={'200'}
+          />
+        </div>
       </div>
     </MainLayout>
   );
---
+++ src/components/parts/InfoCards.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export const InfoCards = ({
+  text,
+  variantBorder,
+  variantText,
+  quantity,
+  ...props
+}) => {
+  return (
+    <>
+      <div className="col-md-4">
+        <div className={`card mb-4 py-3 ${variantBorder} `}>
+          <div className="card-body">
+            <div className="row align-items-center">
+              <h5 className={`m-0 font-weight-bold ${variantText}`}>{text}</h5>
+              <div className="h5 mb-0 m-auto font-weight-bold text-gray-800">
+                {quantity}
+              </div>
+              <div className="col-auto">
+                <i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
